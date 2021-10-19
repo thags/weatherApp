7f765b49dfb9b29e52974451c47d92c7ac7ddabe
Tyler <tyler.hagstrom@gmail.com>
2021-10-19 09:36:03 -0500

    Added the js file to be tracked by git

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("Worked");
+function weatherAPICall(location){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=44568c1fa787c46f7595556abadcb26f`, {mode: 'cors'})
+    .then(response => response.json())
+    .then(data => processWeatherFetch(data));
+}
+
+async function processWeatherFetch(weather){
+    let temp = weather.main.temp;
+
+    console.log(temp)
+}
+
+
+const fweather = weatherAPICall("lenexa");
